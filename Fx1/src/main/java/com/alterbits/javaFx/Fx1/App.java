c834d958e75b8a1557a401def9c5f2323c017beb
vikto <vikto@DESKTOP-LBPUS8P>
2016-11-17 11:33:21 +0100

    uno

---
+++ Fx1/src/main/java/com/alterbits/javaFx/Fx1/App.java
@@ -0,0 +1,13 @@
+package com.alterbits.javaFx.Fx1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
